4924c4e1e0347d0b72032271b0087a89ba9f478f
Anatolii Zaika <anatolii.zka@gmail.com>
2024-03-07 21:02:07 +0300

    Add db context

---
+++ TestSolutionGameOfLife/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace TestSolutionGameOfLife
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(App.Config.GetConnectionString("DbConnection"));
+        }
+    }
+}
