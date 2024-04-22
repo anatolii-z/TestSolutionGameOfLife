e369612dcd1675b3110bc4158fa96b4b7a6de19c
Anatolii Zaika <anatolii.zka@gmail.com>
2024-04-22 23:14:22 +0300

    add baseviewmodel with notifypropertychanged

--- TestSolutionGameOfLife/ViewModels/MainWindowViewModel.cs
+++ TestSolutionGameOfLife/ViewModels/MainWindowViewModel.cs
@@ -1,21 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace TestSolutionGameOfLife.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : BaseViewModel
     {
+        #region Private field
+        private bool _canStart;
+        private bool _canPause;
+        private bool _canRandomGeneration;
+        #endregion
+
         #region Properties
-        public bool CanStart { get; private set; }
-        public bool CanPause { get; private set; }
-        public bool CanGenerate { get; private set; }
+        public bool CanStart
+        {
+            get { return _canStart; } 
+            private set
+            {
+                _canStart = value;
+                OnPropertyChanged(nameof(CanStart));
+            }
+        }
+        public bool CanPause
+        {
+            get { return _canPause; }
+            private set 
+            {
+                _canPause = value;
+                OnPropertyChanged(nameof(CanPause));
+            }
+        }
+        public bool CanGenerate
+        {
+            get { return _canRandomGeneration; }
+            private set
+            {
+                _canRandomGeneration = value;
+                OnPropertyChanged(nameof(CanGenerate));
+            } 
+        }
         #endregion
 
         #region Commands
@@ -27,7 +50,9 @@ namespace TestSolutionGameOfLife.ViewModels
 
         private void LoadedWindow()
         {
-            throw new NotImplementedException();
+            CanStart = true;
+            CanPause = false;
+            CanGenerate = true;
         }
 
         private void StartGame()
